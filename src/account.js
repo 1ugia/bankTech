e29103f49005017ab6148213448ef6f8817c341b
1ugia <82887880+1ugia@users.noreply.github.com>
2021-11-03 13:01:34 +0000

    refactoring code to DRY

--- src/account.js
+++ src/account.js
@@ -4,27 +4,36 @@ class Account {
     this.interactions = [];
   }
   deposit(amount) {
-    if(isNaN(amount)) {
-      throw 'Expected Money';
-    } else if(amount < 0) {
-      throw 'Negative amount'
-    };
+    this._depositError(amount);
     return this.interactions.push(amount);
   }
 
   withdraw(amount) {
-    if(amount > this.balance()) {throw 'No money'
-  } else if(isNaN(amount)) {
-    throw 'Expected Money'
-  } else if(amount < 0) {
-    throw 'Negative amount'
-  };
+    this._withdrawError(amount);
     return this.interactions.push(-amount);
   }
 
   balance(/*function*/) { //logic
     return this.interactions.reduce((a, b) => a + b, 0)
   }
+
+  _depositError(amount) {
+    if(isNaN(amount)) {
+      throw 'Expected Money';
+    } else if(amount < 0) {
+      throw 'Negative amount'
+    };
+  };
+
+  _withdrawError(amount) {
+    if(amount > this.balance()) {throw 'No money'
+    } else if(isNaN(amount)) {
+      throw 'Expected Money'
+    } else if(amount < 0) {
+      throw 'Negative amount'
+    };
+  };
+
 };
 
 module.exports = Account;
