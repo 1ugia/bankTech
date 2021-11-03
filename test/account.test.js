fe6dda95218d9f5e42127741f5a0feaff6bad949
1ugia <82887880+1ugia@users.noreply.github.com>
2021-11-03 12:40:01 +0000

    refactoring tests

--- test/account.test.js
+++ test/account.test.js
@@ -1,68 +1,71 @@
 const Account = require('../src/account');
 
-test('creates an account', () => {
-  let account = new Account
-  expect(account.balance()).toBe(0);
+let account;
+beforeEach(() => {
+  account = new Account();
 });
+
+  describe('Creating account', () => {
+    test('creates an account', () => {
+      expect(account.balance()).toBe(0);
+    });
+  });
   
-test('Puts deposits in', ()=> {
-  let account = new Account
-  account.deposit(50)
-  expect(account.balance()).toBe(50)
-});
+  describe('depositing', () => {
+    test('Puts deposits in', ()=> {
+      account.deposit(50)
+      expect(account.balance()).toBe(50)
+    });
 
-test('it withdraws', ()=> {
-  let account = new Account
-  account.deposit(50)
-  account.withdraw(20)
-  expect(account.balance()).toBe(30)
-});
+    test('deposit integers, not strings', () => {
+      expect(() => {
+        account.deposit('a hundred')
+      }).toThrow('Expected Money')
+    });
+  
+    test('deposit floats', () => {
+      account.deposit(30.5)
+      expect(account.balance()).toBe(30.5)
+    });
 
-test('stops you from withdrawing more than balance', () => {
-  let account = new Account
-  account.deposit(50)
-  expect(() => {
-    account.withdraw(60)
-  }).toThrow('No money')
-});
+    test('cannot deposit minus amounts', () => {
+      expect(() => {
+        account.deposit(-30)
+      }).toThrow('Negative amount')
+    });
+  });
+    
+  describe('withdrawing', () => {
+    test('it withdraws', ()=> {
+      account.deposit(50)
+      account.withdraw(20)
+      expect(account.balance()).toBe(30)
+    });
+  
+    test('stops you from withdrawing more than balance', () => {
+      account.deposit(50)
+      expect(() => {
+        account.withdraw(60)
+      }).toThrow('No money')
+    });
+    
+    test('only withdraws numbers', () => {
+      expect(() => {
+        account.withdraw('a hundred')
+      }).toThrow('Expected Money')
+    });
+  
+    test('withdraws floats', () =>{
+      account.deposit(30)
+      account.withdraw(1.50)
+      expect(account.balance()).toBe(28.50)
+    });
 
-test('deposit integers, not strings', () => {
-  let account = new Account
-  expect(() => {
-    account.deposit('a hundred')
-  }).toThrow('Expected Money')
-});
+    test('cannot withdraw minus amount', () => {
+      expect(() => {
+        account.withdraw(-30)
+      }).toThrow('Negative amount')
+    });
+  });
 
-test('deposit floats', () => {
-  let account = new Account
-  account.deposit(30.5)
-  expect(account.balance()).toBe(30.5)
-});
 
-test('only withdraws numbers', () => {
-  let account = new Account
-  expect(() => {
-    account.withdraw('a hundred')
-  }).toThrow('Expected Money')
-});
-
-test('withdraws floats', () =>{
-  let account = new Account
-  account.deposit(30)
-  account.withdraw(1.50)
-  expect(account.balance()).toBe(28.50)
-});
-
-test('cannot deposit minus amounts', () => {
-  let account = new Account
-  expect(() => {
-    account.deposit(-30)
-  }).toThrow('Negative amount')
-});
-
-test('cannot withdraw minus amount', () => {
-  let account = new Account
-  expect(() => {
-    account.withdraw(-30)
-  }).toThrow('Negative amount')
-});
